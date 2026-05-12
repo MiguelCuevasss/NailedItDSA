0b35da1f1d5dc7ee920704db5310d74c57d5d7cf
Miguel Cuevas <miguel.cuevas@ufm.edu>
2026-05-12 16:53:25 -0600

    Commit 4
    Agregando la función para agregar tarea.

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -6,3 +6,18 @@ class DoublyLinkedList:
         self.ultimoNodo = None
         self.nodoActual = None
         self.tamaño = 0
+
+    def agregarTarea(self, tarea):
+        nuevoNodo = Node(tarea)
+        
+        if self.primerNodo is None:
+            self.primerNodo = nuevoNodo
+            self.ultimoNodo = nuevoNodo
+            self.nodoActual = nuevoNodo
+
+        else:
+            self.ultimoNodo.siguiente = nuevoNodo
+            nuevoNodo.anterior = self.ultimoNodo
+            self.ultimoNodo = nuevoNodo
+
+        self.tamaño += 1
