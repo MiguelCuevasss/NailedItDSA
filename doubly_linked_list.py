8af1b1e3ced35fb5b00dedb325fa930a98856fed
Miguel Cuevas <miguel.cuevas@ufm.edu>
2026-05-12 16:38:42 -0600

    Commit 3
    Agregando las cracterísticas de una lista doblemente linkeada.

---
+++ doubly_linked_list.py
@@ -0,0 +1,8 @@
+from node import Node
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.primerNodo = None
+        self.ultimoNodo = None
+        self.nodoActual = None
+        self.tamaño = 0
